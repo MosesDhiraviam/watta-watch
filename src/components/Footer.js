695b7384abdc6f559fdcd6db203fe2d3b69eb801
Moses Jebaraj Dhiraviam <jebaraj.smart07@gmail.com>
2021-07-15 05:08:57 -0400

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,44 @@
 import React from 'react'
 import "../assets/css/Footer.css"
+import { useHistory } from "react-router-dom";
 
 const Footer = () => {
+    let history = useHistory();
     return (
         <>
             <div className="foot_main">
                         
                 <div className="foot_div1">
                     <ul className ="foot_ul1">
-                        <li className ="content_li"><a className="links" href="/">FAQ</a></li>
-                        <li className ="content_li"><a  className="links" href="/">
+                        <li className ="content_li"><a className="links" onClick={()=>{
+                            history.push("/");
+                        }} >FAQ</a></li>
+                        <li className ="content_li"><a  className="links" onClick={()=>{
+                            history.push("/");
+                        }} >
                             Investor Relations</a></li>
-                        <li className ="content_li"><a className="links"  href="/">Privacy</a></li>
-                        <li className ="content_li"><a  className="links"  href="/">Speed Test</a></li>
+                        <li className ="content_li"><a className="links"  onClick={()=>{
+                            history.push("/");
+                        }} >Privacy</a></li>
+                        <li className ="content_li"><a  className="links"  onClick={()=>{
+                            history.push("/");
+                        }} >Speed Test</a></li>
                     </ul>
                 </div>
 <div className="foot_div2">
                     <ul className ="foot_ul2">
-                    <li className ="content_li"><a  className="links" href="/loginPage">Login</a></li>
-                        <li className ="content_li"><a  className="links" href="/registerPage">Register</a></li>
-                        <li className ="content_li"><a  className="links" href="/moviesPage">Movies Section</a></li>
-                        <li className ="content_li"><a  className="links" href="/tvShowspage">Tv Shows Section</a></li>
+                    <li className ="content_li"><a  className="links" onClick={()=>{
+                            history.push("/loginPage");
+                        }} >Login</a></li>
+                        <li className ="content_li"><a  className="links" onClick={()=>{
+                            history.push("/registerPage");
+                        }} >Register</a></li>
+                        <li className ="content_li"><a  className="links" onClick={()=>{
+                            history.push("/moviesPage");
+                        }} >Movies Section</a></li>
+                        <li className ="content_li"><a  className="links" onClick={()=>{
+                            history.push("/tvShowsPage");
+                        }} >Tv Shows Section</a></li>
                     </ul>
                 </div>
 
