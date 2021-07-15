d130a50f09b40dbc809a5a86d23dd00b0561a0a5
Moses Jebaraj Dhiraviam <jebaraj.smart07@gmail.com>
2021-07-15 12:14:01 -0400

    fix4

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import  '../assets/css/Banner.css';
+//import  '../assets/css/Banner.css';
 import {useState,useEffect} from "react";
+import 'bootstrap/dist/css/bootstrap.css'
+import { Carousel } from 'react-bootstrap';
 
 
 
@@ -49,7 +51,7 @@ const Banner = () => {
     return (
         <>
         
-        <header className = "banner"
+        {/* <header className = "banner"
             style={{
                 backgroundSize: "cover",
                 backgroundImage: `url(${banner.banner})`,
@@ -66,10 +68,31 @@ const Banner = () => {
             
             <div className = "banner__fadeBottom" ></div>
         </header>
-       
+        */}
 
 
-
+<Carousel>
+    <Carousel.Item interval={1000}>
+        <img className = "d-block w-100" src="https://image.tmdb.org/t/p/w300//nVKRspU9SQEs2gNrms8cDKsI4vx.jpg" width = "540" height = "460"
+        alt="car-1"/>
+        <Carousel.Caption><h4>The Tomorrow War</h4></Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item interval={1000}>
+        <img className = "d-block w-100" src="https://image.tmdb.org/t/p/w300//tyZmdMdI3jLs93HDObOZkdq4J7z.jpg" width = "540" height = "460"
+        alt="car-1"/>
+        <Carousel.Caption><h4>Fear Street Part One: 1994</h4></Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item interval={1000}>
+        <img className = "d-block w-100" src="https://image.tmdb.org/t/p/w300//z2UtGA1WggESspi6KOXeo66lvLx.jpg" width = "540" height = "460"
+        alt="car-1"/>
+        <Carousel.Caption><h4>A Quiet Place Part II</h4></Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item interval={1000}>
+        <img className = "d-block w-100" src="https://image.tmdb.org/t/p/w300//ok7RdHhVngnwkvKj09tvtOvypG.jpg" width = "540" height = "460"
+        alt="car-1"/>
+        <Carousel.Caption><h4>No Sudden Move</h4></Carousel.Caption>
+    </Carousel.Item>
+</Carousel>
 
 
 
